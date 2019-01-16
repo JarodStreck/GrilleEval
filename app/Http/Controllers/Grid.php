b18e37258fd854988aafae398d2d971eeae1418e
Jarod Streckeisen <strecky1800@gmail.com>
2019-01-16 08:11:48 +0100

    Update grid info query

--- app/Http/Controllers/Grid.php
+++ app/Http/Controllers/Grid.php
@@ -10,6 +10,7 @@ class Grid extends Controller
         $content = DB::table('criteria_assessment')
         ->join('criteria','criteria_id','=','criteria.id')
         ->join('students','student_id','=','students.id')
+        ->join('evaluation_grids','evaluation_grid_id','=','evaluation_grids.id')
         ->where('evaluation_grid_id','=',$id)
         ->select('nbPoint','maxPoint','description','name')
         ->orderBy('name','question_id','criteria.id')
